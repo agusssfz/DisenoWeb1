c6176e8a6a6933015d5bb6a4a6a0794ca8927e7e
agustin <fagustin651@gmail.com>
2025-10-15 22:23:39 -0300

    se añadieron validacion y tooltips al formulario

--- JS/contacto.js
+++ JS/contacto.js
@@ -1,11 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("contactForm");
+  const nombre = document.getElementById("nombre");
+  const apellido = document.getElementById("apellido");
+
+  [nombre, apellido].forEach((campo) => {
+    campo.addEventListener("input", () => {
+      campo.value = campo.value.replace(/[^A-Za-zÁÉÍÓÚáéíóúÑñ\s]/g, "");
+    });
+  });
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     if (!form.checkValidity()) {
       form.classList.add("was-validated");
+
+      const invalidFields = form.querySelectorAll(":invalid");
+      invalidFields.forEach((field) => {
+        let message = "Campo inválido";
+
+        if (field.validity.valueMissing) {
+          message = "Este campo es obligatorio";
+        } else if (field.validity.typeMismatch && field.type === "email") {
+          message = "Debe ingresar un correo electrónico válido";
+        }
+
+        field.setAttribute("data-bs-toggle", "tooltip");
+        field.setAttribute("data-bs-placement", "right");
+        field.setAttribute("title", message);
+
+        
+        new bootstrap.Tooltip(field);
+      });
+
       return;
     }
 
